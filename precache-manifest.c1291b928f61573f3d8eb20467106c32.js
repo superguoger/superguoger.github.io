20cec8ffad28eed53dea4f99d3c03a5f688d0f24
superguoger <48018300+superguoger@users.noreply.github.com>
2019-03-03 22:19:06 +0800

    啊啊

---
+++ precache-manifest.c1291b928f61573f3d8eb20467106c32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "686e1fd9c5f3e76f39065af9fbb9b927",
+    "url": "/static/media/2.686e1fd9.jpg"
+  },
+  {
+    "revision": "9045509679c38a1297da",
+    "url": "/static/css/main.5e4157e2.chunk.css"
+  },
+  {
+    "revision": "fabea407784eb07454b3",
+    "url": "/static/js/1.fabea407.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5fbcc48115d45d96e3d6908854468309",
+    "url": "/static/media/1.5fbcc481.jpg"
+  },
+  {
+    "revision": "9045509679c38a1297da",
+    "url": "/static/js/main.90455096.chunk.js"
+  },
+  {
+    "revision": "c489237ba4e03e025fa0a9100ad25fe3",
+    "url": "/static/media/3.c489237b.jpg"
+  },
+  {
+    "revision": "225c7fdfc1cf4e315a0a49e34e174334",
+    "url": "/static/media/music1.225c7fdf.jpg"
+  },
+  {
+    "revision": "4de8eb4603338a0aaf0b8b666e289b55",
+    "url": "/static/media/music2.4de8eb46.jpg"
+  },
+  {
+    "revision": "09179d7744bdb11b2e2bc4c0438f6e55",
+    "url": "/static/media/music3.09179d77.jpg"
+  },
+  {
+    "revision": "8af1336b40cb3188ac432994a323ed6f",
+    "url": "/index.html"
+  }
+];
